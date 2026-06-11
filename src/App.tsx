a89ce72ca2b7405e0b560a8caa454e1dc5714b47
Avon Yu <avon.yu@outlook.com>
2026-06-11 21:47:18 +0800

    feat: splash screen on app launch with theme-aware styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { SplashScreen } from "@/components/SplashScreen";
 import "./App.css";
 
 function App() {
+  const [splashVisible, setSplashVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSplashVisible(false), 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="cadence:theme">
+      <SplashScreen visible={splashVisible} />
       <Outlet />
       <Toaster />
     </ThemeProvider>
