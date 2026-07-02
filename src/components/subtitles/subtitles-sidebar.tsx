19eb70d6074ceaf7c149fbceb111dca0de76208e
Avon Yu <avon.yu@outlook.com>
2026-07-02 20:04:02 +0800

    style(subtitles): hide scrollbar in explanation drawer
    Add a .no-scrollbar utility to packaged CSS (per CSP guidance) and apply it to the sentence-explanation drawer's scrollable content.

--- src/components/subtitles/subtitles-sidebar.tsx
+++ src/components/subtitles/subtitles-sidebar.tsx
@@ -104,7 +104,7 @@ export const SubtitlesSidebar = memo(function SubtitlesSidebar({
               </Button>
             </DrawerClose>
           </DrawerHeader>
-          <div className="flex-1 min-h-0 overflow-y-auto px-4 pb-4">
+          <div className="no-scrollbar flex-1 min-h-0 overflow-y-auto px-4 pb-4">
             {drawerProps && (
               <SentenceExplanationPanel
                 sentence={drawerProps.sentence}
